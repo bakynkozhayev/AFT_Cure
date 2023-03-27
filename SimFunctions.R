32ff77d347fad255d12bab7e36e196707148e0d8
Birzhan <birzhik3000@gmail.com>
2023-03-27 18:10:02 +0200

    `cure_frac` investigation

--- SimFunctions.R
+++ SimFunctions.R
@@ -191,9 +191,4 @@ sim_fits <-
     out
   }
 
-RNGkind("L'Ecuyer-CMRG")
-set.seed(1)
-results_cf_0.9 <- sim_fits(cure_frac = 0.9, n_datasets = 100, cores = 30)
-results_cf_0.1 <- sim_fits(cure_frac = 0.1, n_datasets = 100, cores = 30)
-results_cf_0.5 <- sim_fits(cure_frac = 0.5, n_datasets = 100, cores = 30)
-results_cf_0 <- sim_fits(cure_frac = 0, n_datasets = 100, cores = 30)
+
